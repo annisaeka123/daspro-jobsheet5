7dc1e664c678433201036bb68b554ea7c5f2cb76
annisaeka123 <annisaekapuspitaaa@gmail.com>
2023-09-29 08:38:16 +0700

    commit if else

--- PercobaanPemilihan103.java
+++ PercobaanPemilihan103.java
@@ -10,10 +10,8 @@ import java.util.Scanner;
         System.out.print("Masukkan angka: ");
         int angka = input03.nextInt();
 
-        if (angka % 2 == 0)
-            System.out.println("Angka "+angka+" bilangan genap");
-        else
-            System.out.println("Angka "+angka+" bilangan ganjil");
-
+        String status = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+        System.out.println("Angka " + angka + " " + status);
     }
 }
+
